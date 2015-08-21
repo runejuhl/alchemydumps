e8438c6a896551475fa990ea4ae096d5f1abd3ec
Rune Juhl Jacobsen <rune@juhljacobsen.dk>
2015-08-21 21:58:30 +0200

    Should have read^Wunderstood PEP 0440 before pushing.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 
 from setuptools import setup, find_packages
 
-setup(name='Flask-AlchemyDumps',
-      version='0.0.10.gpg',
+setup(name='Flask-AlchemyDumps-GPG',
+      version='0.0.10',
       description='SQLAlchemy backup/dump tool for Flask',
       long_description=open('README.rst').read(),
       classifiers=['Development Status :: 3 - Alpha',
